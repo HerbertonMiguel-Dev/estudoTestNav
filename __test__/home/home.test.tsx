33f9023422633a9ae98e32a62aefaf650e937dbe
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-20 16:48:06 -0300

    criado test para o home

--- __test__/home/home.test.tsx
+++ __test__/home/home.test.tsx
@@ -1,5 +1,8 @@
-import { render } from '@testing-library/react-native'
+import { render, fireEvent, waitFor } from '@testing-library/react-native'
 import { Home } from '../../src/home'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { StackParamList } from '../../App'
 
 
 describe("Test component Home", () => {
@@ -7,6 +10,20 @@ describe("Test component Home", () => {
   it("deve renderizar o texto no componente inicial", () => {
     const { getByText } = render(<Home/>)
     expect(getByText("Página home!")).toBeTruthy();
+
+  })
+
+  it("deve navegar para a tela Perfil quando o botão é pressionado", async () => {
+    const { getByText } = render(<Home/>)
+
+    const button = getByText("Perfil")
+    fireEvent.press(button);
+
+    const { navigate } = useNavigation<NativeStackNavigationProp<StackParamList>>();
+
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalledWith("profile")
+    })
 
   })
 
