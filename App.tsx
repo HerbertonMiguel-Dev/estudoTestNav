40ebe8dcc21b2f8a5af010919a2ee7ed44a163e9
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-21 10:49:39 -0300

    criado Stack.Screen para o User

--- App.tsx
+++ App.tsx
@@ -2,10 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from  '@react-navigation/native-stack'
 import { Home } from './src/home'
 import { Profile } from './src/profile'
+import { User } from './src/user'
 
 export type StackParamList = {
   home: undefined;
   profile: undefined;
+  user: {
+    name: string;
+  }
 }
 
 const Stack = createNativeStackNavigator<StackParamList>()
@@ -21,6 +25,11 @@ export default function App() {
         <Stack.Screen
           name="profile"
           component={Profile}
+        />
+
+        <Stack.Screen
+          name="user"
+          component={User}
         />
       </Stack.Navigator>
     </NavigationContainer>
