dc20ed01aab9a2c5ab944507fa2c1e47cb76d339
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-21 15:17:16 -0300

    criado test para user

--- __test__/user/user.test.tsx
+++ __test__/user/user.test.tsx
@@ -1,5 +1,17 @@
-import { render } from '@testing-library/react-native'
+import { render, fireEvent } from '@testing-library/react-native'
 import { User } from '../../src/user'
+import { decrease } from '../../src/utils/math'
+
+jest.mock("../../src/utils/math", () => {
+  return{ 
+    decrease: jest.fn(),
+  }
+})
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+})
 
 describe("User component tests", () => {
 
@@ -8,6 +20,30 @@ describe("User component tests", () => {
 
     expect(getByText("Página Usuario!")).toBeTruthy();
     expect(getByText("Herberton Miguel")).toBeTruthy()
+
+  })
+
+  it("deve chamar a função handleCalculate", () => {
+    const logSpy = jest.spyOn(console, "log")
+
+    const { getByText } = render(<User/>)
+
+    const buttonCalcular = getByText("Calcular")
+    fireEvent.press(buttonCalcular);
+
+    expect(logSpy).toHaveBeenCalledWith(30)
+
+  })
+
+
+  it("deve chamar a função de diminuição", () => {
+    const { getByText } = render(<User/>)
+
+    const buttonDiminuir = getByText("Diminuir")
+    fireEvent.press(buttonDiminuir);
+
+    expect(decrease).toHaveBeenCalledWith(50, 10)
+
 
   })
 
