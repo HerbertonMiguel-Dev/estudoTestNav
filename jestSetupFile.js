794099c21cca525f448603ef02b285c25ba6e244
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-21 11:15:42 -0300

    criado um mock para suporte do useRoute no contato

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -3,6 +3,8 @@
 export const mockNavigate = jest.fn();
 export const mockGoBack = jest.fn();
 
+global.mockedRouteName = "user";
+
 // Mock do Navigation
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual("@react-navigation/native");
@@ -13,9 +15,20 @@ jest.mock('@react-navigation/native', () => {
       goBack: mockGoBack
     }),
     NavigationContainer: ({ children }) => <>{children}</>,
-    useRoute: () => ({
-      params: { name: "Herberton Miguel" }
-    })
+    useRoute: () => {
+      if(global.mockedRouteName === "user"){
+        return{
+          params: { name: "Herberton Miguel" }
+        }
+      }
+
+      if(global.mockedRouteName === "contato"){
+        return{
+          params: { telefone: "99101010" }
+        }
+      }
+
+    }
   }
 })
 
@@ -28,3 +41,4 @@ jest.mock("@react-navigation/native-stack", () => {
     })
   }
 })
+
