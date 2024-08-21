1d5bb50e98d80848194ef0626d25f5a585aaf3c2
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-21 15:19:05 -0300

    criado button diminuir e calcular importado utils

--- src/user/index.tsx
+++ src/user/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useRoute, RouteProp } from '@react-navigation/native'
+import { decrease } from '../utils/math'
 
 type RouteUserParams = {
   user: {
@@ -13,10 +14,26 @@ type UserRouteProps = RouteProp<RouteUserParams, "user">
 export function User() {
   const route = useRoute<UserRouteProps>();
 
+  function handleCalculate(n1: number, n2: number){
+    const soma = n1 + n2;
+    console.log(soma);
+    return soma; 
+  }
+
  return (
    <View style={styles.container}> 
     <Text>Página Usuario!</Text>
     <Text>{route.params.name}</Text>
+
+    <Button
+      title="Calcular"
+      onPress={() => handleCalculate(10, 20) }
+    />
+
+    <Button
+      title="Diminuir"
+      onPress={() => decrease(50, 10) }
+    />
    </View>
   );
 }
